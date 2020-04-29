905dff6c20e440e21e47a2f385ac0dce000dc2f2
filia alexis <filia.alexis@gmail.com>
2020-04-29 19:22:19 +0200

    Les controllers sont fait

---
+++ app/controllers/solutions_controller.rb
@@ -0,0 +1,10 @@
+class SolutionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'solutions/new'
+  get 'solutions/create'
+  get 'solutions/show'
+  get 'parties/new'
+  get 'parties/create'
+  get 'parties/show'
+  get 'games/new'
+  get 'games/create'
+  get 'games/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
